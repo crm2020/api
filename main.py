eb3ef8ebfa941aa95760460a1c1e2208e7562ad3
crm2020 <chemunshi@outlook.com>
2024-04-24 20:48:04 +0200

    added endpoints
    api is now made in fast api
    following whats going to happen get an ssl for https and fine tune the api by testing

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ app = FastAPI()
 async def read_root():
     return {"Hello": "World"}
 
+@app.get("/api/noob/health")
+async def read_health():
+    return {"status": "ok"}
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get ("api/withdraw")
+async def withdraw(amount: int):
+    return {"amount": amount}
+
+@app.get ("api/accountinfo")
+async def account_info(firstname: str, lastname: str, balance: int):
+    return {"firstname ": firstname, "lastname ": lastname, "balance ": balance}
